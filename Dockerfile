67e89dc2d2a59e1a92045e60d826f7f82d80ecc0
Aamer <aamer@192-168-1-120.tpgi.com.au>
2024-04-12 14:58:39 +1000

    postgres and pgadmin containers are added

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM python:3.11.7-alpine
 WORKDIR /app
 COPY requirements.txt /app/requirements.txt
-RUN apk add --no-cache gcc musl-dev python3-dev libffi-dev openssl-dev cargo
+RUN apk add --no-cache gcc musl-dev python3-dev libffi-dev openssl-dev cargo postgresql-dev
 RUN pip install -r /app/requirements.txt
 COPY . /app
-# Run migrations
-RUN python manage.py migrate
 EXPOSE 8000
-CMD ["python", "/app/manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- core/settings.py
+++ core/settings.py
@@ -93,10 +93,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME", "appdb"),
+        "USER": os.getenv("DATABASE_USER", "appuser"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "apppass"),
+        "HOST": os.getenv(
+            "DATABASE_HOST", "db"
+        ),  # Use service name defined in docker-compose
+        "PORT": "5432",
     }
 }
 
